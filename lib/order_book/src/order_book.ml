d86703215c969cb384dd21b0df1c4339b985e43d
Andrew <an6reww@gmail.com>
2026-06-18 20:21:28 +0000

    Complete find_match and snapshot_side functions with updated comparator function

--- lib/order_book/src/order_book.ml
+++ lib/order_book/src/order_book.ml
@@ -62,6 +62,20 @@ let find t order_id =
   match find_in Buy with Some _ as result -> result | None -> find_in Sell
 ;;
 
+
+
+let create_comparator side =
+  fun order_1 order_2 ->
+    let price_1 = Order.price order_1 in 
+    let price_2 = Order.price order_2 in
+    let time_1 = Order.order_id order_1 in 
+    let time_2 = Order.order_id order_2 in
+
+    if Price.is_more_aggressive side ~price:price_1 ~than:price_2 then 1
+    else if Price.is_more_aggressive side ~price:price_2 ~than:price_1 then -1
+    else if Order_id.(time_1 < time_2) then 1 else -1
+;;
+
 (* NOTE: This walks the list front-to-back and returns the *first* tradable
    order, not the best-priced one. Orders are in reverse insertion order
    (newest first), so this matches against whatever was most recently added,
@@ -71,12 +85,18 @@ let find_match t incoming =
   let incoming_side = Order.side incoming in
   let opposite_side = Side.flip incoming_side in
   let resting_orders = side_list t opposite_side in
-  
-  List.find resting_orders ~f:(fun resting ->
+    
+  let marketable_orders = List.filter resting_orders ~f: (fun resting ->
     Price.is_marketable
       incoming_side
       ~price:(Order.price incoming)
-      ~resting_price:(Order.price resting))
+      ~resting_price:(Order.price resting)) in
+  
+  let reduce_filter order_1 order_2 = 
+    let comparator = create_comparator opposite_side in
+    let result = comparator order_1 order_2 in
+    if result = 1 then order_1 else order_2 in
+  List.reduce marketable_orders ~f:reduce_filter
 ;;
 
 let orders_on_side t side = side_list t side
@@ -85,26 +105,9 @@ let count t side = List.length (side_list t side)
 
 let best_price t side =
   let side_list = side_list t side in
-  let price_list = List.map side_list ~f:Order.price  in
-(* 
-  let is_more_aggressive_helper price than = 
-    if (Price.is_more_aggressive side price than) then price else than in *)
-  List.reduce price_list ~f: (fun price than -> if (Price.is_more_aggressive side price than) then price else than)
-  
-  (* is_more_aggressive_helper *)
+  let price_list = List.map side_list ~f:Order.price in
+  List.reduce price_list ~f: (fun price than -> if (Price.is_more_aggressive side ~price ~than) then price else than)
 ;;
-
-(* 
-  match side_list t side with
-  | [] -> None
-  | first :: rest ->
-    let is_better =
-      match (side : Side.t) with Buy -> Price.( > ) | Sell -> Price.( < )
-    in
-    Some
-      (List.fold rest ~init:(Order.price first) ~f:(fun best order ->
-         let price = Order.price order in
-         if is_better price best then price else best)) *)
 
 let best_level t side : Level.t option =
   match best_price t side with
@@ -124,12 +127,8 @@ let best_bid_offer t : Bbo.t =
 ;;
 
 let snapshot_side t (side : Side.t) =
-  let compare =
-    match side with
-    | Buy -> Comparable.reverse Level.compare
-    | Sell -> Level.compare
-  in
-  orders_on_side t side |> List.map ~f:Level.of_order |> List.sort ~compare
+  let compare = create_comparator side in
+  orders_on_side t side |>  List.sort ~compare |> List.map ~f:Level.of_order 
 ;;
 
 let snapshot t =
