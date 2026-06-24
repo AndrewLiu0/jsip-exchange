ad64455178e91526c363aefc34fb22514237e490
Andrew <an6reww@gmail.com>
2026-06-24 16:32:08 +0000

    migrate order_book to use hashqueue buildable

--- lib/order_book/src/order_book.ml
+++ lib/order_book/src/order_book.ml
@@ -1,68 +1,85 @@
 open! Core
 open Jsip_types
-open Async_log_kernel.Ppx_log_syntax
+(* open Async_log_kernel.Ppx_log_syntax *)
 
+
+module Order_queue = Hash_queue.Make(Order_id)
 type t =
   { symbol : Symbol.t
-  ; mutable bids : Order.t list
-  ; mutable asks : Order.t list
+  ; mutable bids : (Order.t Order_queue.t) Price.Map.t
+  ; mutable asks : (Order.t Order_queue.t) Price.Map.t
+  ; mutable id_to_order: Order.t Order_id.Map.t
+  (*Lazy Removal for log n optimization*)
   }
 [@@deriving sexp_of]
 
-let create symbol = { symbol; bids = []; asks = [] }
+let create symbol = { 
+  symbol; 
+  bids = Price.Map.empty ;
+  asks = Price.Map.empty; 
+  id_to_order = Order_id.Map.empty
+  }
 let symbol t = t.symbol
 
-let side_list t side =
+let side_map t side =
   match (side : Side.t) with Buy -> t.bids | Sell -> t.asks
 ;;
 
-let set_side_list t side orders =
+let set_side_map t side orders =
   match (side : Side.t) with
   | Buy -> t.bids <- orders
   | Sell -> t.asks <- orders
 ;;
 
 let add t order =
+  let price = Order.price order in
   let side = Order.side order in
-  set_side_list t side (order :: side_list t side)
+  let side_map = side_map t side in 
+  let order_id = Order.order_id order in
+  (*Creating new map and updating old map*)
+  t.id_to_order <- Map.set t.id_to_order ~key:order_id ~data:order;
+
+
+  let find_result = Map.find side_map price in 
+  let queue = match find_result with 
+  | Some q -> q
+  | None -> Order_queue.create() in
+  ignore(Order_queue.enqueue_back queue order_id order )
 ;;
 
-let remove' t order_id =
-  let remove_from t side order_id =
-    let orders = side_list t side in
-    match
-      List.partition_tf orders ~f:(fun o ->
-        Order_id.equal (Order.order_id o) order_id)
-    with
-    | [], _ -> None
-    | [ found ], rest ->
-      set_side_list t side rest;
-      Some found
-    | matches, _ ->
-      [%log.info
-        "BUG: More than one order matching order_id found when removing"
-          (order_id : Order_id.t)
-          (matches : Order.t list)
-          (t.symbol : Symbol.t)
-          (side : Side.t)];
-      None
-  in
-  match remove_from t Buy order_id with
-  | Some _ as result -> result
-  | None -> remove_from t Sell order_id
+let get_queue t order_id = 
+  let order = Map.find_exn t.id_to_order order_id in 
+  let side_map = side_map t (Order.side order) in
+  Map.find_exn side_map (Order.price order)
+
+
+
+let remove' t order_id: Order.t option= 
+  let queue = get_queue t order_id in
+  let result = Order_queue.lookup_and_remove queue order_id in
+
+  (* Cleanup from order_book data structures*)
+  let new_id_to_order =  Map.remove t.id_to_order order_id in
+  t.id_to_order <- new_id_to_order;
+  (if Order_queue.length queue = 0
+    then 
+      let order = Map.find_exn t.id_to_order order_id in
+      let side = Order.side order in 
+      let side_map = side_map t side in 
+      let new_map = Map.remove side_map (Order.price order) in
+      set_side_map t side new_map);
+  result
 ;;
 
-let remove t order_id = ignore (remove' t order_id : Order.t option)
+let remove t order_id: unit = 
+  ignore (remove' t order_id)
 
 let find t order_id =
-  let find_in side =
-    List.find (side_list t side) ~f:(fun o ->
-      Order_id.equal (Order.order_id o) order_id)
-  in
-  match find_in Buy with Some _ as result -> result | None -> find_in Sell
+  let queue = get_queue t order_id in
+  Order_queue.lookup queue order_id
 ;;
 
-let create_comparator side order_1 order_2 =
+(* let create_comparator side order_1 order_2 =
   let price_1 = Order.price order_1 in
   let price_2 = Order.price order_2 in
   let time_1 = Order.order_id order_1 in
@@ -74,64 +91,75 @@ let create_comparator side order_1 order_2 =
   else if Order_id.(time_1 < time_2)
   then -1
   else 1
-;;
+;; *)
+
+let find_best_order t side = 
+
+  let resting_orders = side_map t side in
+
+  let best = match side with 
+  | Buy -> Map.max_elt resting_orders 
+  | Sell -> Map.min_elt resting_orders in 
+
+  let open Option.Let_syntax in
+  let %bind (_price, best_queue) = best in 
+  Order_queue.dequeue_front best_queue
+
+
 
 (* NOTE: This walks the list front-to-back and returns the *first* tradable
    order, not the best-priced one. Orders are in reverse insertion order
    (newest first), so this matches against whatever was most recently added,
    regardless of price. See test_matching_engine.ml for a test that
    demonstrates why this is wrong. *)
-let find_match t incoming =
+let find_match t incoming: Order.t option =
   let incoming_side = Order.side incoming in
   let opposite_side = Side.flip incoming_side in
-  let resting_orders = side_list t opposite_side in
-  let marketable_orders =
-    List.filter resting_orders ~f:(fun resting ->
-      Price.is_marketable
-        incoming_side
-        ~price:(Order.price incoming)
-        ~resting_price:(Order.price resting))
-  in
-  let reduce_filter order_1 order_2 =
-    let comparator = create_comparator opposite_side in
-    let result = comparator order_1 order_2 in
-    if result = -1 then order_1 else order_2
-  in
-  (* print_s [%message (incoming : Order.t)]; *)
-  List.reduce marketable_orders ~f:reduce_filter
+  let open Option.Let_syntax in
+  let %bind best_resting_order = find_best_order t opposite_side in
+  if Price.is_marketable incoming_side ~price:(Order.price incoming) ~resting_price:(Order.price best_resting_order) 
+    then Some best_resting_order else None
+
 ;;
 
-let orders_on_side t side = side_list t side
-let is_empty t = List.is_empty t.bids && List.is_empty t.asks
-let count t side = List.length (side_list t side)
+let orders_on_side t side = List.filter (Map.data t.id_to_order) ~f:(fun order -> Side.equal (Order.side order) side )
+let is_empty t = Map.is_empty t.bids && Map.is_empty t.asks
+let count t side = Map.length (side_map t side)
 
 let best_price t side =
-  let side_list = side_list t side in
-  let price_list = List.map side_list ~f:Order.price in
-  List.reduce price_list ~f:(fun price than ->
-    if Price.is_more_aggressive side ~price ~than then price else than)
+  let best_order = find_best_order t side in 
+  match best_order with 
+  | Some order -> Some (Order.price order)
+  | None -> None
 ;;
 
+let queue_to_level t (side: Side.t) (price: Price.t) : Level.t = 
+  let queue = Map.find_exn (side_map t side) price in
+  let total_size = (Order_queue.fold queue ~init:Size.zero ~f:(fun acc order -> Size.(+) acc (Order.remaining_size order))) in 
+  {Level.price = price; size = total_size}
+
+  (* queue = Map.find_exn (side_map t side) price *)
+
 let best_level t side : Level.t option =
-  match best_price t side with
+  let best_price = best_price t side in
+  match best_price with
   | None -> None
   | Some price ->
-    let total_size =
-      List.fold (side_list t side) ~init:Size.zero ~f:(fun acc order ->
-        if Price.equal (Order.price order) price
-        then Size.( + ) acc (Order.remaining_size order)
-        else acc)
-    in
-    Some { price; size = total_size }
+    let best_level = queue_to_level t side price in 
+    Some best_level
 ;;
 
 let best_bid_offer t : Bbo.t =
   { bid = best_level t Buy; ask = best_level t Sell }
 ;;
 
+
+(* TODO: ordering of level_list?? *)
 let snapshot_side t (side : Side.t) =
-  let compare = create_comparator side in
-  orders_on_side t side |> List.sort ~compare |> List.map ~f:Level.of_order
+  let map_on_side = side_map t side in
+  let level_map = Map.mapi map_on_side ~f:(fun ~key:price ~data: _hashqueue -> queue_to_level t side price ) in 
+  let level_list = Map.data level_map in
+  level_list
 ;;
 
 let snapshot t =
@@ -143,5 +171,6 @@ let snapshot t =
 ;;
 
 module For_testing = struct
+  (* let remove = remove *)
   let remove = remove'
 end
