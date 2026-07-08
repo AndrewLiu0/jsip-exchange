ae430deb958503226cc06ae9a16d0be467be50a4
Andrew <an6reww@gmail.com>
2026-07-08 19:21:04 +0000

    Core_bench practice, part 4

--- performance/src/sequences.ml
+++ performance/src/sequences.ml
@@ -1,29 +1,49 @@
 open! Core
 
 module List_seq = struct
-  (* TODO: replace the definition of type t and the implementations of
-     create, set, and get *)
-  type t = unit
+  (* A [ref] so [set] can mutate in place (same trick as [Silly_store]'s list
+     ref): an immutable list can only be "updated" by rebuilding it. *)
+  type t = int list ref
 
-  let create () = ()
-  let set t ~key ~data = ignore (t, key, data)
+  let create () = ref []
 
-  let get t key =
-    ignore (t, key);
-    None
+  let set t ~key ~data =
+    let length = List.length !t in
+    if key < 0 || key > length
+    then
+      raise_s
+        [%message
+          "List_seq.set: index out of range" (key : int) (length : int)]
+    else if key = length
+    then t := List.append !t [ data ]
+    else t := List.mapi !t ~f:(fun i x -> if i = key then data else x)
   ;;
+
+  let get t key = List.nth !t key
 end
 
 module Dynarray_seq = struct
-  (* TODO: replace the definition of type t and the implementations of
-     create, set, and get *)
-  type t = unit
+  (* Stdlib [Dynarray] (OCaml >= 5.2); not shadowed by [Core]. *)
+  type t = int Dynarray.t
 
-  let create () = ()
-  let set t ~key ~data = ignore (t, key, data)
+  let create () = Dynarray.create ()
 
+  let set t ~key ~data =
+    let length = Dynarray.length t in
+    if key < 0 || key > length
+    then
+      raise_s
+        [%message
+          "Dynarray_seq.set: index out of range" (key : int) (length : int)]
+    else if key = length
+    then Dynarray.add_last t data
+    else Dynarray.set t key data
+  ;;
+
+  (* Stdlib [Dynarray.get] raises out of range; our interface wants [None]. *)
   let get t key =
-    ignore (t, key);
-    None
+    if key < 0 || key >= Dynarray.length t
+    then None
+    else Some (Dynarray.get t key)
   ;;
 end
